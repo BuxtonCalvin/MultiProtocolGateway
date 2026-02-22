0a70ebd660685fcdb06ddaac2c8f130339dfbff4
Kevin Burke <BuxtonCalvin@Gmail.com>
2026-02-21 20:17:17 -0800

    rollup policies to ensure data integrity and optimal performance.

--- classes/transports/timescaledb.py
+++ classes/transports/timescaledb.py
@@ -1658,10 +1658,10 @@ class RollupManager:
         """
         Rollup Type
             refresh rollup    start_offset   compress_after   reason
-            1 Hour	          3 hours	     1 day	          Allows a 3-hour window for late data before locking it via compression.
-            1 Day	          3 days	     7 days	          Ensures daily rollups are finalized before compressing.
-            1 Week	          2 weeks	     1 month	      Larger window helps capture any delayed source data updates.
-            1 Month	          3 months	     3 months	      Maximum safety for long-term historical accuracy.
+            1 Hour	          3 hours	     2 days	          Allows a 3-hour window for late data before locking it via compression.
+            1 Day	          3 days	     2 weeks	          Ensures daily rollups are finalized before compressing.
+            1 Week	          3 weeks	     2 months	      Larger window helps capture any delayed source data updates.
+            1 Month	          3 months	     6 months	      Maximum safety for long-term historical accuracy.
         """
 
         # hypertable defaults
