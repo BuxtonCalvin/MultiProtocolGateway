378a23a82634d7d5fd585daeef7c73419f1fea06
Kevin Burke <BuxtonCalvin@Gmail.com>
2026-07-15 19:56:05 -0700

    Fix data type import

--- classes/eg4_metadata.py
+++ classes/eg4_metadata.py
@@ -42,7 +42,6 @@ from dataclasses import dataclass
 from typing import Dict, Protocol
 
 from .protocol_settings import (
-    Data_Type,
     Registry_Type,
     WriteMode,
     protocol_settings,
@@ -774,6 +773,7 @@ def _resolve_eg4_battery_serial(
 
 
 def _make_synthetic_entry(name: str, resolver_name: str, note: str) -> registry_map_entry:
+    from .protocol_settings import Data_Type
     return registry_map_entry(
         registry_type=Registry_Type.HOLDING,
         register=-1,
